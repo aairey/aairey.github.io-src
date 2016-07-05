96addf38630306924d661c3c55f833cd2613b8e4
Andy Airey <airey.andy@gmail.com>
2016-07-06 01:48:44 +0200

    additional settings for flex theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,23 +15,62 @@ DEFAULT_LANG = u'en'
 THEME = "/home/andy/pelican-themes/flex"
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Utopit', 'http://www.utopit.be/'),)
+LINKS = (('Utopit', 'http://www.utopit.be/'),)
 
 # Social widget
-SOCIAL = (('Twitter', 'https://twitter.com/a_airey'),
-          ('LinkedIn', 'https://be.linkedin.com/in/andyairey'),
-          ('GitHub', 'https://github.com/aairey'),)
+SOCIAL = (('twitter', 'https://twitter.com/a_airey'),
+          ('linkedin', 'https://be.linkedin.com/in/andyairey'),
+          ('google', 'https://plus.google.com/u/0/+AndyAire'),
+          ('github', 'https://github.com/aairey'),
+	  ('rss', 'http://aairey.github.io/feeds/all.atom.xml'),)
 
-DEFAULT_PAGINATION = False
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+
+# Flex theme settings
+SITETITLE = AUTHOR
+SITESUBTITLE = u'System Engineer'
+SITEDESCRIPTION = u'%s\'s Thoughts and Writings' % AUTHOR
+SITELOGO = u'//en.gravatar.com/userimage/33572940/d1226e18865ebdb31edc84661c5abaa3.jpeg'
+FAVICON = SITEURL + '/images/favicon.ico'
+BROWSER_COLOR = '#333333'
+PYGMENTS_STYLE = 'monokai'
+
+USE_FOLDER_AS_CATEGORY = True
+MAIN_MENU = True
+
+MENUITEMS = (('Archives', '/archives.html'),
+	     ('Categories', '/categories.html'),
+             ('Tags', '/tags.html'),)
+
+COPYRIGHT_YEAR = 2016
+
+# plugins
+PLUGIN_PATHS = ['/home/andy/pelican-plugins']
+PLUGINS = ['gravatar', 'sitemap']
+
+AUTHOR_EMAIL = 'airey.andy@gmail.com'
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.6,
+        'indexes': 0.6,
+        'pages': 0.5,
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly',
+    }
+}
